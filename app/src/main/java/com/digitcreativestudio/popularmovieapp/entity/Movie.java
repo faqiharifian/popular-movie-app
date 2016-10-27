21a7cc21f32cf76e085ed4c4e2a1263d8c4b83b9
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-27 15:57:51 +0700

    membuat entity movie

---
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/entity/Movie.java
@@ -0,0 +1,62 @@
+package com.digitcreativestudio.popularmovieapp.entity;
+
+/**
+ * Created by faqiharifian on 27/10/16.
+ */
+
+public class Movie{
+    private String title;
+    private String posterPath;
+    private String backdropPath;
+    private String overview;
+    private int voteCount;
+    private float voteAverage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
