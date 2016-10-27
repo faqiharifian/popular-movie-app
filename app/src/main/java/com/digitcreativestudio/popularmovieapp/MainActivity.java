8efe343280cfaf1b9fe7b568bc940925cfd95b36
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-27 16:25:34 +0700

    membuat main activity dengan menu sort

---
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.digitcreativestudio.popularmovieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sort_popular:
+                Toast.makeText(this, "popular", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sort_toprated:
+                Toast.makeText(this, "top rated", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+}
