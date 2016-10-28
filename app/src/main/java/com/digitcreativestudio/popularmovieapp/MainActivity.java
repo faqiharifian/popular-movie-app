f92b2b98448ec5995ab5264a878b1dd22896f097
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-28 15:59:19 +0700

    mengaktifkan sort by popular dan top rated, masih menggunakan dummy data

--- app/src/main/java/com/digitcreativestudio/popularmovieapp/MainActivity.java
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/MainActivity.java
@@ -21,38 +21,32 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.movie_recyclerview) RecyclerView movieRV;
 
     private RecyclerView.LayoutManager mLayoutManager;
-    private MovieAdapter adapter;
+    private MovieAdapter movieAdapter;
     private ArrayList<Movie> movieList= new ArrayList<>();
+
+    private String mType = "popular";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
 
         movieList = new ArrayList<>();
 
-        for(int i = 0; i < 50; i++){
-            Movie movie = new Movie();
-            movie.setTitle("Title movie "+i);
-            movie.setPosterPath("Poster path "+i);
-            movie.setBackdropPath("Backdrop path "+i);
-            movie.setOverview("Overview movie "+i);
-            movie.setVoteCount(i);
-            movie.setVoteAverage(i);
-            movieList.add(movie);
-        }
-
         mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         movieRV.setLayoutManager(mLayoutManager);
         movieRV.setItemAnimator(new DefaultItemAnimator());
-        adapter = new MovieAdapter(MainActivity.this, movieList, new MovieAdapter.OnItemClickListener() {
+        movieAdapter = new MovieAdapter(MainActivity.this, movieList, new MovieAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(Movie movie) {
                 Toast.makeText(MainActivity.this, "Movie clicked "+movie.getTitle(), Toast.LENGTH_SHORT).show();
             }
         });
-        movieRV.setAdapter(adapter);
+        movieRV.setAdapter(movieAdapter);
+
+        getMovieList();
     }
 
     @Override
@@ -65,13 +59,44 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.sort_popular:
-                Toast.makeText(this, "popular", Toast.LENGTH_SHORT).show();
+                mType = "popular";
+                getMovieList();
                 break;
             case R.id.sort_toprated:
-                Toast.makeText(this, "top rated", Toast.LENGTH_SHORT).show();
+                mType = "toprated";
+                getMovieList();
                 break;
         }
 
         return true;
+    }
+
+    private void getMovieList(){
+        movieList.clear();
+        movieAdapter.notifyDataSetChanged();
+        if(mType.equals("toprated")){
+            for(int i = 0; i < 50; i++){
+                Movie movie = new Movie();
+                movie.setTitle("Title movie "+i+" TOP RATED");
+                movie.setPosterPath("Poster path "+i+" TOP RATED");
+                movie.setBackdropPath("Backdrop path "+i+" TOP RATED");
+                movie.setOverview("Overview movie "+i+" TOP RATED");
+                movie.setVoteCount(i);
+                movie.setVoteAverage(i);
+                movieList.add(movie);
+            }
+        }else{
+            for(int i = 0; i < 50; i++){
+                Movie movie = new Movie();
+                movie.setTitle("Title movie "+i+" POPULAR");
+                movie.setPosterPath("Poster path "+i+" POPULAR");
+                movie.setBackdropPath("Backdrop path "+i+" POPULAR");
+                movie.setOverview("Overview movie "+i+" POPULAR");
+                movie.setVoteCount(i);
+                movie.setVoteAverage(i);
+                movieList.add(movie);
+            }
+        }
+        movieAdapter.notifyDataSetChanged();
     }
 }
