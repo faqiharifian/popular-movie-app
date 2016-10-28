85493994787d74a8b01fac31f166deb94845b341
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-28 22:25:10 +0700

    menambah collapsing toolbar layout

--- app/src/main/java/com/digitcreativestudio/popularmovieapp/DetailMovieActivity.java
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/DetailMovieActivity.java
@@ -1,13 +1,24 @@
 package com.digitcreativestudio.popularmovieapp;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.digitcreativestudio.popularmovieapp.entity.Movie;
 
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailMovieActivity extends AppCompatActivity {
     Movie mMovie;
+
+    @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.backdrop_imageview) ImageView backdropImageView;
+
+    @BindColor(android.R.color.transparent) int transparent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,12 @@ public class DetailMovieActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mMovie = getIntent().getParcelableExtra("movie");
+
+        ButterKnife.bind(this);
+
+        collapsingToolbarLayout.setTitle(mMovie.getTitle());
+        collapsingToolbarLayout.setExpandedTitleColor(transparent);
+
     }
 
 }
