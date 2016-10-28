5619d15e253fd02f524fc9bc012af7efaab79492
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-28 15:48:20 +0700

    menambah movie adapter dan mengimplementasi recycler view dengan dummy data

--- app/src/main/java/com/digitcreativestudio/popularmovieapp/MainActivity.java
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/MainActivity.java
@@ -1,19 +1,58 @@
 package com.digitcreativestudio.popularmovieapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.digitcreativestudio.popularmovieapp.adapter.MovieAdapter;
+import com.digitcreativestudio.popularmovieapp.entity.Movie;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.movie_recyclerview) RecyclerView movieRV;
+
+    private RecyclerView.LayoutManager mLayoutManager;
+    private MovieAdapter adapter;
+    private ArrayList<Movie> movieList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        movieList = new ArrayList<>();
+
+        for(int i = 0; i < 50; i++){
+            Movie movie = new Movie();
+            movie.setTitle("Title movie "+i);
+            movie.setPosterPath("Poster path "+i);
+            movie.setBackdropPath("Backdrop path "+i);
+            movie.setOverview("Overview movie "+i);
+            movie.setVoteCount(i);
+            movie.setVoteAverage(i);
+            movieList.add(movie);
+        }
+
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        movieRV.setLayoutManager(mLayoutManager);
+        movieRV.setItemAnimator(new DefaultItemAnimator());
+        adapter = new MovieAdapter(MainActivity.this, movieList, new MovieAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Movie movie) {
+                Toast.makeText(MainActivity.this, "Movie clicked "+movie.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        movieRV.setAdapter(adapter);
     }
 
     @Override
---
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/adapter/MovieAdapter.java
@@ -0,0 +1,94 @@
+package com.digitcreativestudio.popularmovieapp.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.digitcreativestudio.popularmovieapp.R;
+import com.digitcreativestudio.popularmovieapp.entity.Movie;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by faqiharifian on 28/10/16.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    private ArrayList<Movie> arrayList;
+    private OnItemClickListener listener;
+    Context context;
+
+    public MovieAdapter() {
+    }
+
+
+    public MovieAdapter(Context context, ArrayList<Movie> arrayList, OnItemClickListener listener) {
+        this.context = context;
+        this.arrayList = arrayList;
+        this.listener = listener;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final Movie item = arrayList.get(position);
+        holder.bind(arrayList.get(position), listener);
+
+        holder.title.setText(item.getTitle());
+        holder.vote.setText(String.valueOf(item.getVoteAverage()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Movie movie);
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private View parentView;
+        @BindView(R.id.movie_title_textview) TextView title;
+        @BindView(R.id.movie_vote_textview) TextView vote;
+        @BindView(R.id.movie_poster_imageview) ImageView poster;
+
+        public MyViewHolder(View view) {
+            super(view);
+            parentView = view;
+            try {
+                ButterKnife.bind(this, view);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void bind(final Movie item, final OnItemClickListener listener) {
+            parentView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+        }
+    }
+}
