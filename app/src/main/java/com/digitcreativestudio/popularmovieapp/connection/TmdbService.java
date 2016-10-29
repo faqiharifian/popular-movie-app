9311e6819f662e08b7b70fd53c162ee2ccc8c865
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-29 10:11:57 +0700

    menambah tmdb service untuk mengambil popular dan toprated movie

--- app/src/main/java/com/digitcreativestudio/popularmovieapp/connection/TmdbService.java
+++ app/src/main/java/com/digitcreativestudio/popularmovieapp/connection/TmdbService.java
@@ -1,10 +1,23 @@
 package com.digitcreativestudio.popularmovieapp.connection;
 
 import com.digitcreativestudio.popularmovieapp.BuildConfig;
+import com.digitcreativestudio.popularmovieapp.parser.MovieListParser;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
 
 /**
  * Created by faqiharifian on 23/09/16.
  */
 public interface TmdbService {
     String API_KEY = BuildConfig.TMDB_API_KEY;
+    
+    @Headers("Content-Type: application/json")
+    @GET("movie/popular?api_key="+API_KEY)
+    Call<MovieListParser> getPopular();
+
+    @Headers("Content-Type: application/json")
+    @GET("movie/top_rated?api_key="+API_KEY)
+    Call<MovieListParser> getTopRated();
 }
